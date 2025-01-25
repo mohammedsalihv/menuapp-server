61fc1fcfde7a660d20124703114db542ca812131
mohammedsalihv <127475695+mohammedsalihv@users.noreply.github.com>
2025-01-26 05:20:16 +0530

    Update Db.js

--- Database/Db.js
+++ Database/Db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-const Database = mongoose.connect(process.env.MONGO);
+const Database = mongoose.connect('mongodb+srv://ezpanowhiffotp:x3aumh0Z2VIUZFrr@cluster0.q9upt.mongodb.net/?retryWrites=true&w=majority&appName=menuManager');
 Database.then(() => {
   console.log("Database connected successfully");
 }).catch((error) => {
